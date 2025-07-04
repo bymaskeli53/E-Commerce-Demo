07b77fb297eafca00370cb43799bb8a08a475316
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2025-07-04 19:01:17 +0300

    feat: Add Profile screen
    This commit introduces the Profile screen, including its fragment, layout, and navigation setup.
    
    Key changes:
    - Created `ProfileFragment.kt` with basic structure and view binding.
    - Added `fragment_profile.xml` layout file with a placeholder TextView.
    - Added `ic_profile.png` drawable for the profile icon.
    - Added a "Profile" item to the bottom navigation menu in `menu_bottom_nav.xml`.
    - Added `profileFragment` to the navigation graph in `nav_graph.xml`.

---
+++ app/src/main/java/com/gundogar/e_commerce_demo/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,31 @@
+package com.gundogar.e_commerce_demo.presentation.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gundogar.e_commerce_demo.R
+import com.gundogar.e_commerce_demo.databinding.FragmentProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
+    }
+}
